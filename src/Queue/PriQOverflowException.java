04989e089aba607e235d86e1a72823c67253ba9c
Mark <mark.rivac@gmail.com>
2016-11-02 16:43:52 -0600

    Agregar Clases Grafo

---
+++ src/Queue/PriQOverflowException.java
@@ -0,0 +1,14 @@
+package Queue;
+
+class PriQOverflowException extends RuntimeException
+{
+  public PriQOverflowException()
+  {
+    super();
+  }
+
+  public PriQOverflowException(String message)
+  {
+    super(message);
+  }
+}
