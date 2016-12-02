dd87b052effe2d1ee0e8aaa425259a10dd8d0371
Mark <mark.rivac@gmail.com>
2016-12-02 16:18:02 -0600

    Final commit

--- src/planISC/isc.java
+++ src/planISC/isc.java
@@ -7,6 +7,9 @@ import java.awt.EventQueue;
  * @author Ilse Monserrat Sánchez Genereux and Mark Octavio Rivera Acosta
  * @version 1.0
  * @since 2016-12-01
+ * 
+ * This is the main class of the project.
+ * It initialize the first window of the program.
  */
 public class isc {
 	
