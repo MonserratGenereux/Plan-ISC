7aca796a5c710a07262b9a6f39f1b7a30ae7dfc8
Monserrat Genereux <monserratgenereux@gmail.com>
2016-12-02 16:16:42 -0600

    Maybe final

--- src/planISC/Welcome.java
+++ src/planISC/Welcome.java
@@ -1,4 +1,4 @@
-
+package planISC;
 import javax.swing.JFrame;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
@@ -15,9 +15,7 @@ import java.awt.event.ActionEvent;
  * The Welcome class creates the first window in the
  * program.
  */
-public class Welcome {
-<<<<<<< HEAD
-    
+public class Welcome {    
     JFrame frame;
     private JTextField txtName;
     private JTextField txtA;
@@ -54,7 +52,7 @@ public class Welcome {
         btnEnter.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 if(e.getSource()==btnEnter){
-                    Remedials window = new Remedials();
+                    Subjects window = new Subjects();
                     window.frame.setVisible(true);
                     frame.setVisible(false);
                 }
@@ -107,99 +105,5 @@ public class Welcome {
         frame.getContentPane().setLayout(groupLayout);
     }
     
-=======
 
-	JFrame frame;
-	private JTextField txtName;
-	private JTextField txtA;
-
-
-	/**
-	 * Launch the application.
-	 */
-	
-
-	public Welcome() {
-		initialize();
-	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 237);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		JLabel lblWelcomeToIsc = new JLabel("Welcome to ISC");
-		lblWelcomeToIsc.setFont(new Font("Lucida Grande", Font.PLAIN, 58));
-		
-		JLabel lblName = new JLabel("Name:");
-		
-		
-		JLabel lblStudentNumber = new JLabel("Student Number:");
-		
-		JButton btnEnter = new JButton("Enter");
-		btnEnter.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if(e.getSource()==btnEnter){
-					Subjects window = new Subjects();
-					window.frame.setVisible(true);
-					frame.setVisible(false);
-				}
-			}
-		});
-		
-		txtName = new JTextField();
-		txtName.setColumns(10);
-		
-		txtA = new JTextField();
-		txtA.setColumns(10);
-		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
-		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addContainerGap()
-							.addComponent(lblWelcomeToIsc))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(21)
-							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-								.addGroup(groupLayout.createSequentialGroup()
-									.addComponent(lblStudentNumber)
-									.addPreferredGap(ComponentPlacement.UNRELATED)
-									.addComponent(txtA, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-								.addGroup(groupLayout.createSequentialGroup()
-									.addComponent(lblName)
-									.addPreferredGap(ComponentPlacement.UNRELATED)
-									.addComponent(txtName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))))
-					.addContainerGap(11, Short.MAX_VALUE))
-				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
-					.addContainerGap(368, Short.MAX_VALUE)
-					.addComponent(btnEnter)
-					.addContainerGap())
-		);
-		groupLayout.setVerticalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(lblWelcomeToIsc)
-					.addGap(32)
-					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblName)
-						.addComponent(txtName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addGap(18)
-					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblStudentNumber)
-						.addComponent(txtA, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(btnEnter)
-					.addContainerGap(12, Short.MAX_VALUE))
-		);
-		frame.getContentPane().setLayout(groupLayout);
-	}
-	
->>>>>>> origin/master
 }
